49ea249ac26887ba7f89a319d2d016d55bb03c7d
GhostDragonRaider <sancii5427@gmail.com>
2025-10-05 20:57:05 +0200

    második próbálkozás

--- JS/script.js
+++
@@ -1,8 +0,0 @@
-const hamburgerNavButton = document.querySelector(".hamburger-nav-button");
-const navButtonLines = document.querySelectorAll(".nav-button-line");
-const navBar = document.querySelector(".nav-bar");
-
-hamburgerNavButton.addEventListener("click", () => {
-    hamburgerNavButton.classList.toggle("active");
-    navBar.classList.toggle("active");
-})
